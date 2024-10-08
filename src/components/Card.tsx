34d9cc7b448bc169ea71ff7425ff4fdf1fc00235
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2024-10-08 21:33:40 +0900

    supabaseのデータ取得

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,8 +1,36 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { getAllUsers } from "../utils/supabaseFunction";
+import { Text } from "@chakra-ui/react";
+import { User } from "../domain/User";
 
 const Card = () => {
   const { id } = useParams();
-  return <div>id:{id}</div>;
+  const [userData, setuserData] = useState<User[]>([]);
+
+  useEffect(() => {
+    const getUserData = async () => {
+      const userDatas = await getAllUsers();
+      if (Array.isArray(userDatas)) {
+        setuserData(userDatas);
+      } else {
+        console.log("Error");
+      }
+    };
+    getUserData();
+  }, []);
+  return (
+    <div>
+      id:{id}
+      {userData.map((user) => {
+        return (
+          <div>
+            <Text>{user.name}</Text>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Card;
