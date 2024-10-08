3f29e34b4f00e052e3bb7828b3af3f27f91aacb4
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2024-10-08 21:03:45 +0900

    supabaseの設定

--- src/utils/supabaseFunction.ts
+++ src/utils/supabaseFunction.ts
@@ -1,3 +1,4 @@
+import { User } from "../domain/User";
 import { supabase } from "./supabase";
 
 export const getAllUsers = async () => {
@@ -7,5 +8,9 @@ export const getAllUsers = async () => {
     return new Error("Error fetching users");
   }
 
-  const usersData = data.map((user) => {});
+  const usersData = data.map((user) => {
+    return new User(user.id, user.name, user.description, user.github_id, user.qiita_id, user.x_id);
+  });
+
+  return console.log(usersData);
 };
