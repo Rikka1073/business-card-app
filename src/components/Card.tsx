cd8327eca0d2ffe63d1681de705eead9ea858ae2
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2024-10-11 17:14:53 +0900

    loading画面の処理追加

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,39 +1,51 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
 import { getAllUsers } from "../utils/supabaseFunction";
-import { Text } from "@chakra-ui/react";
 import { User } from "../domain/User";
+
+import { Box, Text } from "@chakra-ui/react";
+import { useParams } from "react-router-dom";
 
 const Card = () => {
   const { id } = useParams();
   const [userData, setuserData] = useState<User[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const getUserData = async () => {
       const userDatas = await getAllUsers();
       if (Array.isArray(userDatas)) {
         setuserData(userDatas);
+        setLoading(false);
       } else {
         console.log("Error");
       }
     };
     getUserData();
   }, []);
+
   return (
     <div>
       id:{id}
-      {userData.map((user) => {
-        return (
-          <div key={user.id}>
-            <Text>{user.name}</Text>
-            <Text>{user.description}</Text>
-            <Text>{user.user_id}</Text>
-            <Text>{user.github_id}</Text>
-            <Text>{user.qiita_id}</Text>
-            <Text>{user.x_id}</Text>
-          </div>
-        );
-      })}
+      <Box>
+        {loading ? (
+          <Box>
+            <Text>Loading...</Text>
+          </Box>
+        ) : (
+          userData.map((user) => {
+            return (
+              <div key={user.id}>
+                <Text>{user.name}</Text>
+                <Text>{user.description}</Text>
+                <Text>{user.user_id}</Text>
+                <Text>{user.github_id}</Text>
+                <Text>{user.qiita_id}</Text>
+                <Text>{user.x_id}</Text>
+              </div>
+            );
+          })
+        )}
+      </Box>
     </div>
   );
 };
