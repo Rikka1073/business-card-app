a6a45cd6949d03058adcc200801ece5c796f2951
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2024-10-15 20:52:06 +0900

    Register配下のルート設定

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Cards from "../components/Cards";
 import Home from "../components/Home";
 import Card from "../components/Card";
+import Register from "../components/Register";
 
 const Router = () => {
   return (
@@ -9,6 +10,7 @@ const Router = () => {
       <Route path="/" element={<Home />} />
       <Route path="/cards" element={<Cards />} />
       <Route path="/cards/:id" element={<Card />} />
+      <Route path="/cards/register" element={<Register />} />
     </Routes>
   );
 };
