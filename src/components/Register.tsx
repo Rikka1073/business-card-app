15dfbf0726f0704dafaf9c1c26e3a5d3c7a038c6
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2024-10-17 22:22:11 +0900

    各state設定 onchage設定

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,13 +1,37 @@
 import { Box, Button, Center, FormControl, Input, Select, Text } from "@chakra-ui/react";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 
 const Register = () => {
+  const [id, setId] = useState("");
+  const [name, setName] = useState("");
+  const [introduce, setIntroduce] = useState("");
+  const [skill, setSkill] = useState("");
+  const [githubId, setGithubId] = useState("");
+  const [qiitaId, setQiitaId] = useState("");
+  const [xId, setXId] = useState("");
+
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm({ mode: "onBlur" });
   const onsubmit = (data) => console.log(data);
+
+  const onclickEntry = () => {
+    console.log("登録ボタンが押されました");
+  };
+
+  const onchangeId = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value);
+    setId(e.target.value);
+  };
+
+  const onchangeName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value);
+    setName(e.target.value);
+  };
+
   return (
     <Box position="relative" overflow-y="auto">
       <Center>
@@ -22,14 +46,24 @@ const Register = () => {
               <Text pl="2" mb="1">
                 ID
               </Text>
-              <Input {...register("id", { required: true })} placeholder="ID" mb="2" />
+              <Input
+                {...register("id", { required: true, onChange: onchangeId })}
+                placeholder="ID"
+                mb="2"
+                value={id}
+              />
               {errors.id && <Text color="red.500">IDは必須です</Text>}
             </Box>
             <Box mb="5">
               <Text pl="2" mb="1">
                 名前
               </Text>
-              <Input {...register("name", { required: true })} placeholder="名前" mb="2" />
+              <Input
+                {...register("name", { required: true, onChange: onchangeName })}
+                placeholder="名前"
+                value={name}
+                mb="2"
+              />
               {errors.name && <Text color="red.500">名前は必須です</Text>}
             </Box>
             <Box mb="5">
@@ -73,7 +107,7 @@ const Register = () => {
               </Text>
               <Input placeholder="X Id" />
             </Box>
-            <Button colorScheme="teal" mt="5" w="full">
+            <Button colorScheme="teal" mt="5" w="full" onClick={onclickEntry}>
               登録
             </Button>
           </FormControl>
