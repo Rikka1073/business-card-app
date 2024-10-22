8ad9f45fe7821cb92b0d1735865a1a4f94ca3966
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2024-10-22 23:34:34 +0900

    id検索画面作成

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,14 @@
-import { Box, Button, ChakraProvider } from "@chakra-ui/react";
+import { Box, Button, FormLabel, Input, Text } from "@chakra-ui/react";
 
 const Home = () => {
   return (
     <>
-      <Box>
-        Home
+      <Box w="full" bg="red.400" textAlign="center" px="10">
+        <Text as="h2" fontSize="2xl" fontWeight="bold" mb="5">
+          デジタル名刺アプリ
+        </Text>
+        <FormLabel>ID</FormLabel>
+        <Input mb="5" placeholder="ID"></Input>
         <Button colorScheme="blue">Button</Button>
       </Box>
     </>
