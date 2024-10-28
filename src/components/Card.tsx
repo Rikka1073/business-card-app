8107f3f46141cae81cfca53c7ce8aac26aedc023
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2024-10-28 22:07:47 +0900

    スタイル修正

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,8 +3,10 @@ import { getAllUsersData } from "../utils/supabaseFunction";
 import { User } from "../domain/User";
 
 import { Box, Button, Link, Text } from "@chakra-ui/react";
+import { FaGithub } from "react-icons/fa";
+import { SiQiita } from "react-icons/si";
+import { FaXTwitter } from "react-icons/fa6";
 import { useParams } from "react-router-dom";
-import { ExternalLinkIcon } from "@chakra-ui/icons";
 import * as DOMPurify from "isomorphic-dompurify";
 
 import parse from "html-react-parser";
@@ -42,7 +44,7 @@ const Card = () => {
 
   return (
     <Box bg="red.300" h="100vh" display="flex" alignItems="center" justifyContent="center">
-      {id}
+      {/* {id} */}
       <Box boxShadow="xs" p="6" rounded="md" bg="white" w="80%">
         {loading ? (
           <Box>
@@ -52,36 +54,36 @@ const Card = () => {
           userData.map((user, index) => {
             return (
               <Box key={index}>
-                <Text data-testid="userName">ID:{user.user_id}</Text>
-                <Text>名前:{user.name}</Text>
-                <Box>
+                <Text data-testid="userName" mb={5}>
+                  ID:{user.user_id}
+                </Text>
+                <Text mb={5}>名前:{user.name}</Text>
+                <Box data-testid="userDescription" mb={5}>
                   自己紹介:
                   <ParserSanitized html={user.description} />
                 </Box>
-                <Text>スキル:{user.skill}</Text>
-                <Text>
-                  Github:
-                  <Link href={`https://github.com/${user.github_id}`} isExternal>
-                    {user.github_id}
-                    <ExternalLinkIcon mx="2px" />
-                  </Link>
+                <Text data-testid="userSkill" mb={5}>
+                  スキル:{user.skill}
                 </Text>
-                <Text>
-                  Quiita:
-                  <Link href={`https://qiita.com/${user.qiita_id}`} isExternal>
-                    {user.qiita_id}
-                    <ExternalLinkIcon mx="2px" />
-                  </Link>
-                </Text>
-                <Text>
-                  X:
-                  <Link href={`https://x.com/${user.x_id}`} isExternal>
-                    {user.x_id}
-                    <ExternalLinkIcon mx="2px" />
-                  </Link>
-                </Text>
+                <Box display="flex" justifyContent="space-between" mb={5}>
+                  <Text>
+                    <Link href={`https://github.com/${user.github_id}`} isExternal>
+                      <FaGithub size={40} />
+                    </Link>
+                  </Text>
+                  <Text>
+                    <Link href={`https://qiita.com/${user.qiita_id}`} isExternal>
+                      <SiQiita size={40} />
+                    </Link>
+                  </Text>
+                  <Text>
+                    <Link href={`https://x.com/${user.x_id}`} isExternal>
+                      <FaXTwitter size={40} />
+                    </Link>
+                  </Text>
+                </Box>
                 <Link href={`/`} isExternal>
-                  <Button>戻る</Button>
+                  <Button w="full">戻る</Button>
                 </Link>
               </Box>
             );
