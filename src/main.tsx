2e0f2c42c6393a2ed30dd6a3851fb9e6ce79cd42
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2024-11-03 02:26:35 +0900

    import修正

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
-import App from "./App.tsx";
+import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
 
 createRoot(document.getElementById("root")!).render(
